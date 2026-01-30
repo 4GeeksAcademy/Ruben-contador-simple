7676aad943a250229e2a4db00dbcbd7115d82474
Ruben Alba Gonzalez <39350323+Ruben-Alba-Gonzalez@users.noreply.github.com>
2026-01-30 20:01:19 +0000

    Añadidos estilos y un mensaje secreto

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -8,7 +8,7 @@ import TimeCounter from "./TimeCounter"
 //create your first component
 const Home = ({contador}) => {
 	return (
-		<div className="text-center d-flex justify-content-center align-items-center min-vh-100">
+		<div className="text-center d-flex justify-content-center align-items-center min-vh-100 bg-body-secondary">
 			<TimeCounter counter={contador}/>
 		</div>
 	);
--- src/js/components/TimeCounter.jsx
+++ src/js/components/TimeCounter.jsx
@@ -1,8 +1,12 @@
-const TimeCounter= ({counter})=>{
+const TimeCounter = ({ counter }) => {
 
 
     return (
-        <p>Tiempo: {counter[0].toString().length<2? "0"+counter[0] : counter[0]}:{counter[1].toString().length<2? "0"+counter[1] : counter[1]}:{counter[2].toString().length<2? "0"+counter[2] : counter[2]}</p>
+        <div>
+            <h1>Tiempo desperdiciado en esta página</h1>
+            <p className="fs-1">{counter[0].toString().length < 2 ? "0" + counter[0] : counter[0]}:{counter[1].toString().length < 2 ? "0" + counter[1] : counter[1]}:{counter[2].toString().length < 2 ? "0" + counter[2] : counter[2]}</p>
+            {counter[0]>0? <h1 className="text-danger">¡Vete ya de aquí a hacer algo de verdad!</h1>:""}
+        </div>
     );
 };
 
